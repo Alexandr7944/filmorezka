d76e02d43dd6971206e66f0725a544565b2c92a8
Alexander Vasnev <a-s-vasnev88@yandex.ru>
2023-04-21 12:34:19 +0300

    merge movieItems

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ const Home = () => {
         />
         <MoviesList
           title='Современные мультфильмы'
-          url='https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=1'
+          url='https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=2'
         />
       </div>
     </MyContainer>
