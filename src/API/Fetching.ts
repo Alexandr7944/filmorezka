c1ad47f7556cd5bf297df7abb4d36dd8eec155e8
Alexander Vasnev <a-s-vasnev88@yandex.ru>
2023-05-09 10:14:57 +0300

    change fetching

--- src/API/Fetching.ts
+++ src/API/Fetching.ts
@@ -7,7 +7,6 @@ class Fetching {
     let result;
     try {
       const response = await fetch(url, {
-        mode: 'no-cors',
         method: method,
         headers: {
             'X-API-KEY': 'bd41c576-b9b8-455e-b3fa-dc54c04dc1dd',
