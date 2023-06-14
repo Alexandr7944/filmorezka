b85727b623fe4de9ad43c9dfe7ad668e15b46571
Mari <mstanyukova@gmail.com>
2023-06-15 00:19:19 +0200

    added stories

---
+++ src/stories/UI/TabBar_footer.stories.tsx
@@ -0,0 +1,17 @@
+import FooterMobile from '../../components/Footer/FooterMobile';
+import { Meta, StoryObj  } from '@storybook/react';
+
+
+const meta: Meta<typeof FooterMobile> = {
+  title: 'Footer/FooterMobile',
+  component: FooterMobile
+}
+
+export default meta;
+type Story = StoryObj<typeof FooterMobile>;
+
+
+
+export const Primary: Story = {
+    
+  };
