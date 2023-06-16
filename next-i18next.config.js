26f10a792b5e7e4e67e0ba07a1410faa5d6b971b
Mari <mstanyukova@gmail.com>
2023-06-16 15:18:05 +0200

    fixed build errors #5

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,9 @@
+const path = require('path');
 module.exports = {
 	i18n: {
 		locales: ["en", "ru"],   
 		defaultLocale: "ru",
 		localeDetection: false
-	  }
+	  },
+	  localePath: path.resolve('./src/locales'),
 }
